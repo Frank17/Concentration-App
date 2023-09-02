5d596cc49b991f3ef9900a6763bc3d64bfedadb6
Frank Zhang <frankzhang314159@gmail.com>
2023-09-02 11:30:53 -0700

    Updated GPT cache

--- app/api/_gpt_cache.py
+++ app/api/_gpt_cache.py
@@ -2,11 +2,12 @@ from rapidfuzz import fuzz
 from ._nltk_handler import STOPWORDS
 
 import re
+from datetime import datetime, timedelta
 from typing import Callable
 
 
 ALPHABET_REGEX = re.compile('[^a-z]+')
-
+MIN = timedelta(0, 60)
 
 def is_simi(text_a: str, text_b: str) -> bool:
     return fuzz.ratio(text_a, text_b) >= 75
@@ -23,7 +24,7 @@ class judgment_cache:
     """A simple TTL cache that saves URLs with
        GPT-made judgements
 
-       Item: {url: (text, judgment)}
+       Item: {url: (judgment, start_time, keywords, text)}
     """
     
     def __init__(self, requester: Callable):
@@ -34,10 +35,19 @@ class judgment_cache:
     def __call__(self, url: str, text: str, keywords: list[str]) -> str:
         if (
             (record := self._cache.get(url)) and
-            keywords == record[1] and
-            is_simi(record[2], get_clean_text(text))
+            datetime.now() - record[1] < MIN and
+            keywords == record[2] and
+            is_simi(record[3], get_clean_text(text))
         ):
             return record[0]
+        
+        if len(self._cache) % 50 == 0:
+            self._cache.clear()
             
-        self._cache[url] = (self.requester(url, text, keywords), keywords, text)
+        self._cache[url] = (
+            self.requester(url, text, keywords),  # judgment
+            datetime.now(),  # start time
+            keywords,
+            text
+        )
         return self._cache[url][0]
